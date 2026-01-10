936db785006621a2885aa0320465ec8f1061a17d
AbdulmalikAlayande <114596864+AbdulmalikAlayande@users.noreply.github.com>
2026-01-10 11:11:21 +0100

    Add basic configuration classes: CorsConfig and AsyncConfig

---
+++ src/main/java/app/bola/esportsscoutingtool/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package app.bola.esportsscoutingtool.config;
+
+
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+public class AsyncConfig {
+
+}
---
+++ src/main/java/app/bola/esportsscoutingtool/config/CorsConfig.java
@@ -0,0 +1,7 @@
+package app.bola.esportsscoutingtool.config;
+
+public class CorsConfig {
+
+
+
+}
