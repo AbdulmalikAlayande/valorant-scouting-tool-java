0897e7d47018ce167aee45813edfdfb48019d5fc
AbdulmalikAlayande <114596864+AbdulmalikAlayande@users.noreply.github.com>
2026-01-10 22:18:31 +0100

    Add @SuperBuilder and @NoArgsConstructor annotations to ReportStatusResponse for improved object construction and compatibility

--- src/main/java/app/bola/esportsscoutingtool/dto/ReportStatusResponse.java
+++ src/main/java/app/bola/esportsscoutingtool/dto/ReportStatusResponse.java
@@ -1,10 +1,13 @@
 package app.bola.esportsscoutingtool.dto;
 
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 
 @Getter
+@SuperBuilder
+@NoArgsConstructor
 @AllArgsConstructor
 public class ReportStatusResponse {
     private Long requestId;
