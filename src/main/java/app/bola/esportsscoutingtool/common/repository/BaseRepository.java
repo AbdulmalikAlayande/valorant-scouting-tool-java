fbb3b8238fc07c31d4e28d13f2a25b118ca5bc6b
AbdulmalikAlayande <114596864+AbdulmalikAlayande@users.noreply.github.com>
2026-01-10 22:18:05 +0100

    Annotate BaseRepository with @NonNull for entity and ID types to enforce null-safety

--- src/main/java/app/bola/esportsscoutingtool/common/repository/BaseRepository.java
+++ src/main/java/app/bola/esportsscoutingtool/common/repository/BaseRepository.java
@@ -1,11 +1,12 @@
 package app.bola.esportsscoutingtool.common.repository;
 import app.bola.esportsscoutingtool.common.model.BaseModel;
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.io.Serializable;
 
 @NoRepositoryBean
-public interface BaseRepository<ENT extends BaseModel, ID extends Serializable> extends JpaRepository<ENT, ID> {
+public interface BaseRepository<ENT extends BaseModel, ID extends Serializable> extends JpaRepository<@NonNull ENT, @NonNull ID> {
 
 }
