d9b85ac2e7bf69c95481d12d0ab1d006faef2ccc
AbdulmalikAlayande <114596864+AbdulmalikAlayande@users.noreply.github.com>
2026-01-10 11:04:53 +0100

    Add method to find ScoutingReport by ReportRequest ID in repository

--- src/main/java/app/bola/esportsscoutingtool/repository/ScoutingReportRepository.java
+++ src/main/java/app/bola/esportsscoutingtool/repository/ScoutingReportRepository.java
@@ -3,10 +3,13 @@ package app.bola.esportsscoutingtool.repository;
 import app.bola.esportsscoutingtool.common.repository.BaseRepository;
 import app.bola.esportsscoutingtool.model.ScoutingReport;
 
+import java.util.Optional;
+
 /**
  * Repository for {@link ScoutingReport}
  * Repository class for managing scouting reports in the Esports Scouting Tool application.
  */
 public interface ScoutingReportRepository extends BaseRepository<ScoutingReport, Long> {
-
+	
+	Optional<ScoutingReport> findByReportRequestId(Long requestId);
 }
