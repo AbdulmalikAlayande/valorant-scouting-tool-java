7c23b25e1243ca2690f9aaa84059cbe8eabc51b1
AbdulmalikAlayande <114596864+AbdulmalikAlayande@users.noreply.github.com>
2026-01-10 22:18:16 +0100

    Annotate BaseRepository with @NonNull for entity and ID types to enforce null-safety

--- src/main/java/app/bola/esportsscoutingtool/controller/ReportController.java
+++ src/main/java/app/bola/esportsscoutingtool/controller/ReportController.java
@@ -1,13 +1,15 @@
 package app.bola.esportsscoutingtool.controller;
 
+import app.bola.esportsscoutingtool.dto.ReportStatusResponse;
+import app.bola.esportsscoutingtool.dto.ScoutingReportResponse;
 import app.bola.esportsscoutingtool.service.ReportSubmissionService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.NonNull;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 
 @Slf4j
@@ -19,9 +21,22 @@ public class ReportController {
 	final ReportSubmissionService reportSubmissionService;
 	
 	@PostMapping("/submit")
-	public ResponseEntity<Object> submitReport(@RequestBody Object reportRequest) {
+	public ResponseEntity<@NonNull ReportStatusResponse> submitReport(@RequestBody Object reportRequest) {
 		log.info("Received report submission: {}", reportRequest);
 		log.info(reportSubmissionService.toString());
-		return ResponseEntity.ok().body("Report submitted successfully");
+		return ResponseEntity.ok().body(ReportStatusResponse.builder().build());
 	}
+	
+	@GetMapping("/{id}/status")
+	public ResponseEntity<@NonNull ReportStatusResponse> getReportStatus(@PathVariable String id) {
+		log.info("Received report status request for report id: {}", id);
+		return ResponseEntity.ok().body(ReportStatusResponse.builder().build());
+	}
+	
+	@GetMapping("{id}")
+	public CompletableFuture<ResponseEntity<@NonNull ScoutingReportResponse>> getReport(@PathVariable String id) {
+		log.info("Received report request for report id: {}", id);
+		return CompletableFuture.supplyAsync(() -> ResponseEntity.ok().body(ScoutingReportResponse.builder().build()));
+	}
+	
 }
