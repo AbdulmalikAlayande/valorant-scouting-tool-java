94896bc1c703747de7f9ec372dea2570e93221fe
AbdulmalikAlayande <114596864+AbdulmalikAlayande@users.noreply.github.com>
2026-01-10 22:18:46 +0100

    Remove redundant @NoArgsConstructor from ReportSubmissionService

--- src/main/java/app/bola/esportsscoutingtool/service/ReportSubmissionService.java
+++ src/main/java/app/bola/esportsscoutingtool/service/ReportSubmissionService.java
@@ -1,13 +1,11 @@
 package app.bola.esportsscoutingtool.service;
 
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@NoArgsConstructor
 @AllArgsConstructor
 public class ReportSubmissionService {
 
