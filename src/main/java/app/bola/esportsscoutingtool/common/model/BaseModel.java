50a92dd0c4b217147dc861969bf6692b1c1bbac3
AbdulmalikAlayande <114596864+AbdulmalikAlayande@users.noreply.github.com>
2026-01-10 01:40:10 +0100

    Add ReportController with base API mapping

--- src/main/java/app/bola/esportsscoutingtool/common/model/BaseModel.java
+++ src/main/java/app/bola/esportsscoutingtool/common/model/BaseModel.java
@@ -3,6 +3,8 @@ package app.bola.esportsscoutingtool.common.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @MappedSuperclass
 public class BaseModel {
@@ -11,5 +13,13 @@ public class BaseModel {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String publicId;
+	@Column(name = "created_at", nullable = false)
+	private LocalDateTime createdAt = LocalDateTime.now();
+	@Column(name = "created_at", nullable = false)
+	private LocalDateTime lastModifiedAt = LocalDateTime.now();
 	
+	@PrePersist
+	protected void onCreate() {
+		publicId = java.util.UUID.randomUUID().toString();
+	}
 }
