6f59586981e8a775580e4995cbcdcf68d84c46c9
AbdulmalikAlayande <114596864+AbdulmalikAlayande@users.noreply.github.com>
2026-02-03 10:43:24 +0100

    Add `findByPublicId` method to BaseRepository for querying entities by publicId

--- src/main/java/app/bola/cloud9stratigenai/common/repository/BaseRepository.java
+++ src/main/java/app/bola/cloud9stratigenai/common/repository/BaseRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.io.Serializable;
+import java.util.Optional;
 
 @NoRepositoryBean
 public interface BaseRepository<ENT extends BaseModel, ID extends Serializable> extends JpaRepository<@NonNull ENT, @NonNull ID> {
 
+	Optional<ENT> findByPublicId(String publicId);
 }
